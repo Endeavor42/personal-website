6f6705baf7718729017b808919392f77976094de
Fitri <fitrirozi1@gmail.com>
2021-02-13 11:44:43 -0600

    completed about me content

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,6 +5,9 @@ import downloadIcon from "../images/about_me/download.svg";
 import computerIcon from "../images/about_me/computer.svg";
 import blob1Icon from "../images/about_me/blob1.svg";
 import blob2Icon from "../images/about_me/blob2.svg";
+import globeIcon from "../images/about_me/globe.svg";
+import minionFrontIcon from "../images/about_me/minion_front.svg";
+import minionBackIcon from "../images/about_me/minion_back.svg";
 
 function AboutMe() {
   return (
@@ -23,6 +26,33 @@ function AboutMe() {
         <img className="blob1" src={blob1Icon} alt="blob" />
         <img className="computerIcon" src={computerIcon} alt="Computer" />
         <img className="blob2" src={blob2Icon} alt="blob" />
+      </div>
+      <div className="aboutMe__textWrapper">
+        <div className="titleWrapper">
+          <h1>Hi, I'm Fitri</h1>
+          <p>Software Developer + UI/UX Designer</p>
+          <hr />
+        </div>
+        <div className="description">
+          <p>
+            I am a tech-savvy, self-driven developer who loves to work on
+            cutting-edge software technologies. I am currently a junior majoring
+            in Computer Science at Wichita State University.
+          </p>
+        </div>
+      </div>
+      <div className="aboutMe__bottomImgWrapper">
+        <img className="aboutMe__globeIcon" src={globeIcon} alt="globe" />
+        <img
+          className="aboutMe__minion--front"
+          src={minionFrontIcon}
+          alt="minion"
+        />
+        <img
+          className="aboutMe__minion--back"
+          src={minionBackIcon}
+          alt="minion"
+        />
       </div>
     </div>
   );
