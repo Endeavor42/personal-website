5620a886a051690a7badfb271498bf05eb8346a4
Fitri <fitrirozi1@gmail.com>
2021-02-28 17:26:56 -0600

    navbar tags design fished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,46 @@
 import "../styles/navbar.scss";
-import hamburgerIcon from "../images/about_me/hamburger.svg";
-import downloadImg from "../images/about_me/download.svg";
+import hamburgerIcon from "../images/navbar/hamburger.svg";
+import downloadImg from "../images/navbar/download.svg";
+import hightlightTags from "../images/navbar/highlight-tags.svg";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar__container">
+        <ul className="navbar__links">
+          <li>
+            <p>About me</p>
+            <div className="tagsWrapper">
+              <div className="item tag1"></div>
+              <div className="item tag2" />
+              <div className="item tag3" />
+            </div>
+          </li>
+          <li>
+            Experience
+            <div className="tagsWrapper">
+              <div className="item tag1" />
+              <div className="item tag2" />
+              <div className="item tag3" />
+            </div>
+          </li>
+          <li>
+            Skills
+            <div className="tagsWrapper">
+              <div className="item tag1" />
+              <div className="item tag2" />
+              <div className="item tag3" />
+            </div>
+          </li>
+          <li>
+            Projects
+            <div className="tagsWrapper">
+              <div className="item tag1" />
+              <div className="item tag2" />
+              <div className="item tag3" />
+            </div>
+          </li>
+        </ul>
         <img src={hamburgerIcon} alt="Hamburger Icon" />
         <a href="../Fitri_Rozi_Resume.pdf" download>
           <img src={downloadImg} alt="Download resume" />
