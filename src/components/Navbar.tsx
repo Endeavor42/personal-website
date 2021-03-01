c571ca1bb77b288a423874e99e8ed13edbb7ff44
Fitri <fitrirozi1@gmail.com>
2021-02-28 19:40:08 -0600

    managed to add tags to nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,104 @@
+import { useState } from "react";
 import "../styles/navbar.scss";
 import hamburgerIcon from "../images/navbar/hamburger.svg";
 import downloadImg from "../images/navbar/download.svg";
-import hightlightTags from "../images/navbar/highlight-tags.svg";
 
 function Navbar() {
+  const [linkState, setLinkState] = useState("about me");
+
   return (
     <div className="navbar">
       <div className="navbar__container">
         <ul className="navbar__links">
-          <li>
-            <p>About me</p>
-            <div className="tagsWrapper">
+          <li onClick={() => setLinkState("about me")}>
+            {
+              <p
+                style={
+                  linkState === "about me"
+                    ? { color: "white", textDecoration: "none" }
+                    : { color: "black" }
+                }
+              >
+                About me
+              </p>
+            }
+            <div
+              style={
+                linkState === "about me"
+                  ? { visibility: "visible" }
+                  : { visibility: "hidden" }
+              }
+              className="tagsWrapper"
+            >
               <div className="item tag1"></div>
               <div className="item tag2" />
               <div className="item tag3" />
             </div>
           </li>
-          <li>
-            Experience
-            <div className="tagsWrapper">
+          <li onClick={() => setLinkState("experience")}>
+            <p
+              style={
+                linkState === "experience"
+                  ? { color: "white", textDecoration: "none" }
+                  : { color: "black" }
+              }
+            >
+              Experience
+            </p>
+            <div
+              style={
+                linkState === "experience"
+                  ? { visibility: "visible" }
+                  : { visibility: "hidden" }
+              }
+              className="tagsWrapper"
+            >
               <div className="item tag1" />
               <div className="item tag2" />
               <div className="item tag3" />
             </div>
           </li>
-          <li>
-            Skills
-            <div className="tagsWrapper">
+          <li onClick={() => setLinkState("skills")}>
+            <p
+              style={
+                linkState === "skills"
+                  ? { color: "white", textDecoration: "none" }
+                  : { color: "black" }
+              }
+            >
+              Skills
+            </p>
+            <div
+              style={
+                linkState === "skills"
+                  ? { visibility: "visible" }
+                  : { visibility: "hidden" }
+              }
+              className="tagsWrapper"
+            >
               <div className="item tag1" />
               <div className="item tag2" />
               <div className="item tag3" />
             </div>
           </li>
-          <li>
-            Projects
-            <div className="tagsWrapper">
+          <li onClick={() => setLinkState("projects")}>
+            <p
+              style={
+                linkState === "projects"
+                  ? { color: "white", textDecoration: "none" }
+                  : { color: "black" }
+              }
+            >
+              Projects
+            </p>
+            <div
+              style={
+                linkState === "projects"
+                  ? { visibility: "visible", textDecoration: "none" }
+                  : { visibility: "hidden" }
+              }
+              className="tagsWrapper"
+            >
               <div className="item tag1" />
               <div className="item tag2" />
               <div className="item tag3" />
